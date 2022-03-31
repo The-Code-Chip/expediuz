52c7cd39add11fbefd8f6e9eb28448010526bf31
Sourashis Ghosh Roy <service.codechip@gmail.com>
2022-03-31 21:20:27 +0530

    0.1

---
+++ pages/components/navbar.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Styles from "../../styles/Pages.module.css";
+import Logo from "../../public/assets/images/vectors/Expediuz Logo (White).svg";
+
+export default function navbar() {
+  return (
+    <>
+      <nav className={Styles.NavBar}>
+        <span className={Styles.NavLogo}>
+          <Image
+            className={Styles.LogoImg}
+            src={Logo}
+            alt="Expediuz Logo"
+            width={72}
+            height={72}
+          />
+        </span>
+        <h2 className={Styles.ExpediuzBranding}>
+          Expediuz Information Services
+        </h2>
+
+        <ul className={Styles.NavUl}>
+          <li className={Styles.NavLi}>
+            <Link href="">Home</Link>
+          </li>
+          <li className={Styles.NavLi}>
+            <Link href="">Our Services</Link>
+          </li>
+          <li className={Styles.NavLi}>
+            <Link href="">Our Portfolio</Link>
+          </li>
+          <li className={Styles.NavLi}>
+            <Link href="">Learn To Code</Link>
+          </li>
+          <li className={Styles.NavLi}>
+            <Link href="">Contact Us</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
