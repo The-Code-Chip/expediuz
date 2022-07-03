2649472d9044b6243134b700145b3602fc034955
Sourashis Ghosh Roy <service.codechip@gmail.com>
2022-07-03 15:33:57 +0530

    3.9

--- pages/firebase.jsx
+++ pages/firebase.jsx
@@ -1,14 +1,15 @@
+import firebase from "firebase";
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBQOLRWePgDeIoHFPq6Wjl87A5VD-VJdvk",
-    authDomain: "expediuz-thecodechip.firebaseapp.com",
-    projectId: "expediuz-thecodechip",
-    storageBucket: "expediuz-thecodechip.appspot.com",
-    messagingSenderId: "221027561245",
-    appId: "1:221027561245:web:45c65eddc1c96182507bf4",
-    measurementId: "G-K4YW1PMNJ4"
+  apiKey: "AIzaSyBQOLRWePgDeIoHFPq6Wjl87A5VD-VJdvk",
+  authDomain: "expediuz-thecodechip.firebaseapp.com",
+  projectId: "expediuz-thecodechip",
+  storageBucket: "expediuz-thecodechip.appspot.com",
+  messagingSenderId: "221027561245",
+  appId: "1:221027561245:web:45c65eddc1c96182507bf4",
+  measurementId: "G-K4YW1PMNJ4",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,17 +18,18 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 const SignInWithGoogle = () => {
-    signInWithPopup(auth, provider).then((result) => {
-        const UserName = result.user.displayName;
-        const UserEmail = result.user.email;
-        const UserProfilePic = result.user.photoURL;
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const UserName = result.user.displayName;
+      const UserEmail = result.user.email;
+      const UserProfilePic = result.user.photoURL;
 
-        localStorage.setItem("UserName", UserName);
-        localStorage.setItem("UserEmail", UserEmail);
-        localStorage.setItem("UserProfilePic", UserProfilePic);
-
-    }).catch((error) =>{
-        alert(error)
-        console.log(error);
+      localStorage.setItem("UserName", UserName);
+      localStorage.setItem("UserEmail", UserEmail);
+      localStorage.setItem("UserProfilePic", UserProfilePic);
+    })
+    .catch((error) => {
+      alert(error);
+      console.log(error);
     });
-}
+};
